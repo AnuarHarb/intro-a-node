6fe6f9a9cd81633919f718584847aa6ba3b6242f
Anuar Harb <anuarharb@hotmail.com>
2019-02-08 19:22:58 -0600

    version 1

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 var cowsay = require('cowsay');
+var introduccion = require('./introduccion');
 
 let nombre = "Anuar";
+let edad = 5;
+let villano = "Edgar";
 let fraceVaquita = "Surcaré los 7 mares de pasto";
 
 let historia = "Hola, mi nombre es " + nombre + ". Y hoy me enconté con una vaquita pitara que decía: " + fraceVaquita;
 
-console.log(historia);
+console.log(introduccion.historia(nombre, edad, villano));
+console.log(nudo.historia(nombre, edad, villano));
+console.log(desenlace.historia(nombre, edad, villano));
 
 console.log(cowsay.say({
-  text : fraceVaquita,
+  text : "Hola Esther, ¡Soy una vaquita pirtata!",
   e : "x-o",
   T : "v "
 }));
