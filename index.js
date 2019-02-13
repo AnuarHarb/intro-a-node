b136393c23611852f1e484003b9e074cc1822b03
Anuar Harb <anuarharb@hotmail.com>
2019-02-12 19:59:30 -0600

    cree mi propia rama

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-var cowsay = require('cowsay');
+const cowsay = require('cowsay');
+const fs = require('fs');
 const intro = require('./intro');
+const final = require('./final');
 
-let personaje = "Anuar";
-let villano = "Juan";
-let lugar = "Centraal";
-let hobby = "Programar";
+const personaje = "Anuar";
+const edad = Math.floor(Math.random() * 100);
+const villano = "Juan";
+const lugar = "Centraal";
+const hobby = "Programar";
 
-console.log(intro.historia(personaje, lugar));
+const introHistoria = intro.historia(personaje, lugar);
+const finalHistoria = final.historia(lugar, hobby);
+
+const historiaCompleta = introHistoria + finalHistoria;
+
+console.log(historiaCompleta);
+
+// muchasPalabras // Camel Case
+// muchas-palabras // dash case
+// muchas_palabras //snake case
 
 console.log(cowsay.say({
   text : 'hola',
