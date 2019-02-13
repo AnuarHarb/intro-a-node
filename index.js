d96ff28fc50cc4d35186ec2a6fa5ae624f1f6329
Anuar Harb <anuarharb@hotmail.com>
2019-02-12 21:45:52 -0600

    historia completa archivo creado

--- index.js
+++ index.js
@@ -1,20 +1,49 @@
+// Modulos
 const cowsay = require('cowsay');
 const fs = require('fs');
 const intro = require('./intro');
 const final = require('./final');
 
+//Variables de generacion
 const personaje = "Anuar";
 const edad = Math.floor(Math.random() * 100);
 const villano = "Juan";
 const lugar = "Centraal";
 const hobby = "Programar";
 
-const introHistoria = intro.historia(personaje, lugar);
-const finalHistoria = final.historia(lugar, hobby);
+fs.readFile('personajes.txt', 'utf-8', function(err, contenido){
+  if(err) {
+    throw err;
+  }
 
-const historiaCompleta = introHistoria + finalHistoria;
+  const personajes = contenido.split(',');
+  const numeroPesonaje = Math.floor(Math.random() * personajes.length);
+  const personajeRandom = personajes[numeroPesonaje];
 
-console.log(historiaCompleta);
+  const historiaCompleta = juntarHistoria(personajeRandom);
+
+  //Escribir historia
+  console.log(historiaCompleta);
+
+  fs.writeFile('historia.txt', historiaCompleta, function(err) {
+    if(err) {
+      throw err;
+    }
+  });
+
+});
+
+// Partes de historia
+function juntarHistoria(personaje) {
+  const introHistoria = intro.historia(personaje, lugar);
+  const finalHistoria = final.historia(personaje, lugar);
+  const historiaCompleta = introHistoria + finalHistoria;
+  return historiaCompleta;
+}
+
+
+
+
 
 // muchasPalabras // Camel Case
 // muchas-palabras // dash case
